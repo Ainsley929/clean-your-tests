4a6e04119209e876d3a2e1d3699bf90f7a995a50
Ainsley <cfrey26@gmail.com>
2019-12-05 20:22:24 -0500

    homework

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,14 @@
 const { expect } = require('chai')
+const chai = require('chai')
 const { describe, it } = require('mocha')
 const employee = require('./employee')
 const products = require('./products')
 const pricing = require('../pricing')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+
+chai.use(sinonChai)
 
 describe('pricing', () => {
   describe('formatPrice', () => {
@@ -10,37 +16,60 @@ describe('pricing', () => {
       const formattedPrice = pricing.formatPrice(15.335)
       expect(formattedPrice).to.equal(15.33)
     })
-
-    describe('getEmployerContribution', () => {
-      it('returns the contribution amount from the product when the contribution type is dollars', () => {
-        const contribution = pricing.getEmployerContribution(products.longTermDisability.employerContribution, 15.33)
-        expect(contribution).to.equal(10)
-      })
-      it('returns the calculated contribution (percent of price) when the contribution type is a percentage', () => {
-        const contribution = pricing.getEmployerContribution(products.longTermDisability.employerContribution, 15.33)
-        expect(contribution).to.equal(10)
-      })
+    it('returns the price with two decimal places when a whole number is provided', () => {
+      const formattedPrice = pricing.formatPrice(15)
+      expect(formattedPrice).to.equal(15.00)
     })
   })
+
+  describe('getEmployerContribution', () => {
+    it('returns the contribution amount from the product when the contribution type is dollars', () => {
+      const contribution = pricing.getEmployerContribution(products.longTermDisability.employerContribution, 15.33)
+
+      expect(contribution).to.equal(10)
+    })
+    it('returns the calculated contribution (percent of price) when the contribution type is a percentage', () => {
+      const contribution = pricing.getEmployerContribution(products.voluntaryLife.employerContribution, 100)
+
+      expect(contribution).to.equal(10)
+    })
+  })
+
   describe('calculateVolLifePricePerRole', () => {
-    it('returns the price of vol life coverage for a particular role (ee)', () => {
-      //define test data
+    it('returns the price of vol life coverage for a particular role (ee) without employer contribution', () => {
+
       const coverageLevel = [{ role: 'ee', coverage: 125000 }]
-      //call the func under test with data
+
       const price = pricing.calculateVolLifePricePerRole('ee', coverageLevel, products.voluntaryLife.costs)
-      //expect good things
+
       expect(price).to.equal(43.75)
     })
   })
+
   describe('calculateVolLifePrice', () => {
     it('returns the price (pre employer contribution) for vol life product based on user selection', () => {
+      const selectedOptions = {
+        familyMembersToCover: ['ee'],
+        coverageLevel: [
+          { role: 'ee', coverage: 125000 }
+        ]
+      }
+
+      const price = pricing.calculateVolLifePrice(products.voluntaryLife, selectedOptions)
+
+      expect(price).to.equal(43.75)
+    })
+
+    it('returns the price (pre employer contribution) for vol life product for ee and sp', () => {
       const selectedOptions = {
         familyMembersToCover: ['ee', 'sp'],
         coverageLevel: [
           { role: 'ee', coverage: 200000 },
-          { role: 'sp', coverage: 75000 }]
+          { role: 'sp', coverage: 75000 },
+        ],
       }
       const price = pricing.calculateVolLifePrice(products.voluntaryLife, selectedOptions)
+
       expect(price).to.equal(79)
     })
   })
@@ -54,14 +83,6 @@ describe('pricing', () => {
       const price = pricing.calculateLTDPrice(products.longTermDisability, employee, selectedOptions)
 
       expect(price).to.equal(32.04)
-    })
-    it('returns false if the role is not ee', () => {
-      const selectedOptions = {
-        familyMembersToCover: ['sp']
-      }
-      const price = pricing.calculateLTDPrice(products.longTermDisability, employee, selectedOptions)
-
-      expect(price).to.equal(0)
     })
   })
 
@@ -97,11 +118,10 @@ describe('pricing', () => {
 
       expect(price).to.equal(22.04)
     })
+  })
+  it('throws an error on unknown product type', () => {
+    const unknownProduct = { type: 'vision' }
 
-    it('throws an error on unknown product type', () => {
-      const unknownProduct = { type: 'vision' }
-
-      expect(() => pricing.calculateProductPrice(unknownProduct, {}, {})).to.throw('Unknown product type: vision')
-    })
+    expect(() => pricing.calculateProductPrice(unknownProduct, {}, {})).to.throw('Unknown product type: vision')
   })
 })
